2c16f327ba2e43c559ed5f54aab0fdf08792f85a
Matheus-git <mathiew0@gmail.com>
2025-03-27 21:15:02 -0300

    first commit

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,13 @@
+return {
+	{
+		'nvim-treesitter/nvim-treesitter',
+		run = ':TSUpdate',
+		config = function()
+			require'nvim-treesitter.configs'.setup {
+				ensure_installed = {"lua", "rust", "yaml", "php", "blade"}, -- Instalar linguagens mantidas
+				highlight = { enable = true },    -- Ativar o destaque de sintaxe
+				indent = { enable = true },       -- Ativar indentação com treesitter
+			}
+		end
+	}
+}
