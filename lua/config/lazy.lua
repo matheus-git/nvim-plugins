349a74562485f55d02f970937c68a83a75ea4f32
Matheus-git <mathiew0@gmail.com>
2025-05-04 09:13:20 -0300

    feat: remove update checker

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -37,7 +37,6 @@ require("lazy").setup({
   -- colorscheme that will be used when installing plugins.
   install = { colorscheme = { "habamax" } },
   -- automatically check for plugin updates
-  checker = { enabled = true },
 })
 -- Mason Setup
 require("mason").setup({
