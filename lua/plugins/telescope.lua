e760157a13b992374360ff9c24a7ef2c69d2d61f
Matheus-git <mathiew0@gmail.com>
2025-06-10 20:00:30 -0300

    feat: fix live grep

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,7 +4,19 @@ return {
 		tag = '0.1.8',
 		dependencies = { 'nvim-lua/plenary.nvim' },
 		config = function()
-			local builtin = require('telescope.builtin')
+			local builtin = require('telescope.builtin').setup({
+				defaults = {
+				    file_ignore_patterns = {
+					"^%.",           
+					"node_modules/",
+					"%.git/",      
+					"dist/",      
+					"build/",    
+					"vendor/"
+				    },
+				},
+			    })
+
 			vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
 			vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
 			vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
