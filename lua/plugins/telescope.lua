f3c101b1b178ce5955aa41ad8bcd306a98fbde43
Matheus-git <mathiew0@gmail.com>
2025-06-11 20:25:21 -0300

    fix: telescoppe setup

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,27 +4,31 @@ return {
 		tag = '0.1.8',
 		dependencies = { 'nvim-lua/plenary.nvim' },
 		config = function()
-			local builtin = require('telescope.builtin').setup({
-				defaults = {
-				    file_ignore_patterns = {
-					"^%.",           
-					"node_modules/",
-					"%.git/",      
-					"dist/",      
-					"build/",    
-					"vendor/"
-				    },
-				},
-			    })
+		local telescope = require('telescope')
 
-			vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-			vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
-			vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
-			vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
-			vim.keymap.set('n', '<leader>fgf', builtin.git_files, { desc = 'Telescope git files' })
-			vim.keymap.set('n', '<leader>fgb', builtin.git_branches, { desc = 'Telescope git branches' })
-			vim.keymap.set('n', '<leader>fgs', builtin.git_status, { desc = 'Telescope git status' })
-			vim.keymap.set('n', '<leader>fc', builtin.colorscheme, { desc = 'Telescope colors' })
+		  telescope.setup({
+		    defaults = {
+		      file_ignore_patterns = {
+			"^%.",           
+			"node_modules/",
+			"%.git/",      
+			"dist/",      
+			"build/",    
+			"vendor/"
+		      },
+		    },
+		  })
+
+		  local builtin = require('telescope.builtin')
+
+		  vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+		  vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+		  vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+		  vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+		  vim.keymap.set('n', '<leader>fgf', builtin.git_files, { desc = 'Telescope git files' })
+		  vim.keymap.set('n', '<leader>fgb', builtin.git_branches, { desc = 'Telescope git branches' })
+		  vim.keymap.set('n', '<leader>fgs', builtin.git_status, { desc = 'Telescope git status' })
+		  vim.keymap.set('n', '<leader>fc', builtin.colorscheme, { desc = 'Telescope colors' })
 		end,
 	}
 }
