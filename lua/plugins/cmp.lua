0a78ce23440e9c11678c18be8589769e6a46b427
Matheus-git <mathiew0@gmail.com>
2026-01-03 02:24:12 +0000

    automplete

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -3,9 +3,12 @@ return {
     "hrsh7th/nvim-cmp",
     dependencies = {
       "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",  
       "L3MON4D3/LuaSnip",
       "saadparwaiz1/cmp_luasnip",
     },
+
     config = function()
       local cmp = require("cmp")
       local luasnip = require("luasnip")
@@ -19,33 +22,19 @@ return {
 
         mapping = cmp.mapping.preset.insert({
           ["<C-Space>"] = cmp.mapping.complete(),
-          ["<CR>"]      = cmp.mapping.confirm({ select = true }),
-          ["<Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_next_item()
-            elseif luasnip.expand_or_jumpable() then
-              luasnip.expand_or_jump()
-            else
-              fallback()
-            end
-          end, { "i", "s" }),
-
-          ["<S-Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_prev_item()
-            elseif luasnip.jumpable(-1) then
-              luasnip.jump(-1)
-            else
-              fallback()
-            end
-          end, { "i", "s" }),
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+          ["<Tab>"] = cmp.mapping.select_next_item(),
+          ["<S-Tab>"] = cmp.mapping.select_prev_item(),
         }),
 
         sources = cmp.config.sources({
           { name = "nvim_lsp" },
+          { name = "buffer" }, 
+          { name = "path" },
           { name = "luasnip" },
         }),
       })
     end,
-  }
+  },
 }
+
